66962bbcbdc125f6f02591de9bb34e107ca57507
albusdemens <mannaro85@gmail.com>
2017-09-29 23:03:27 +0200

    Added a few comments to the code

--- Reconstruct3D_ART.m
+++ Reconstruct3D_ART.m
@@ -1,7 +1,7 @@
 close all; clear;
 
 % Load as input output files from reconstr.m
-F1 = load('rec_ART.mat');
+F1 = load('rec_all_angles.mat');
 R = F1.rec;
 
 R_bin = zeros(size(R));
@@ -10,11 +10,12 @@ for i = 1:size(R,3)
     layer = squeeze(R(:,:,i));
     layer1 = layer;
     % Threshold and binarize
-    layer1(layer1 < 45) = 0;
+    layer1(layer1 < 65) = 0;
     layer2 = zeros(size(layer1));
     layer2(layer1 > 0) = 1;
     % Fill holes
     layer2 = imfill(layer2);
+    
     % To get rid of artefacts, erode and dilate
     se = strel('disk',10);
     l_ed = imdilate(imerode(layer2,se),se);
@@ -25,6 +26,8 @@ for i = 1:size(R,3)
     % figure; imagesc(imoverlay(layer, P, [.3 1 .3]));
 end
 
+% Make a volume where voxels have information on their completeness and on
+% their orientation (mu and gamma values)
 X = zeros(nnz(R_bin), 3);
 num_p = 0;
 for aa = 1:size(R,1)
@@ -42,7 +45,8 @@ end
 
 %figure; scatter3(X(:,1), X(:,2), X(:,3));
 %xlabel('X'); ylabel('Y'); zlabel('Z');
-
+ 
+% Project the reconstructed volume along an axis
 S = zeros(size(R_bin,1), size(R_bin,2));
 for i = 1:size(R_bin,2)
     for j = 1:size(R_bin,1)
@@ -52,7 +56,8 @@ for i = 1:size(R_bin,2)
     end
 end
 
-figure; h = pcolor(S); shading flat;
+% Plot the projection
+figure; h = pcolor(imrotate(S, 270)); shading flat;
 
 save('Binary_vol_ART.mat', 'R_bin');
 
